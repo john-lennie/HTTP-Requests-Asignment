6e425b71bd9421833fbd064ee5426f604786c0ec
John <johnanthonylennie@gmail.com>
2017-06-05 23:42:25 +0000

    added printing-buffered-strings.js

---
+++ printing-buffered-strings.js
@@ -0,0 +1,35 @@
+function getAndPrintHTMLChunks () {
+
+  var https = require('https');
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  // notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+  https.get(requestOptions, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    var allData = [];
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      allData.push(data);
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log(allData);
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+getAndPrintHTMLChunks();
