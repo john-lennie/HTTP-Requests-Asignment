337a60e617fa094a18163d4a835d5878f56adef5
John <johnanthonylennie@gmail.com>
2017-06-06 00:08:00 +0000

    altered printing-inchunk.js to output data in chunks

--- printing-inchunks.js
+++ printing-inchunks.js
@@ -16,7 +16,7 @@ function getAndPrintHTMLChunks () {
 
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      console.log('Chunk Received. Length:', data.length);
+      console.log(data + '\n');
     });
 
     // the callback is invoked when all of the data has been received
